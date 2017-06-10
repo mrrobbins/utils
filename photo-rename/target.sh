a2ef8b83faf277ca08a92160385259ce92d91a7f
Matthew Robbins <matthew.r.robbins@gmail.com>
2017-06-10 12:59:20 -0400

    Removing photo-rename scripts that were moved to separate repo

--- photo-rename/target.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Running toLower.sh"
-. ~/GitHub/utils/photo-rename/toLower.sh
-echo "Running renameImageByDate.sh"
-. ~/GitHub/utils/photo-rename/renameImageByDate.sh
--- photo-rename/toLower.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for f in *; do mv "$f" "`echo $f | tr "[:upper:]" "[:lower:]"`"; done
